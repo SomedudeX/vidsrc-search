ccb09fc8df22b51ba401bd946fa8f692c1285e21
Zichen <zichenziv@icloud.com>
2024-04-11 19:21:26 -0700

    Added version and several other python files into utils.py

--- src/vidsrc_search/utils.py
+++ src/vidsrc_search/utils.py
@@ -1,25 +1,31 @@
 import os
 import sys
+import json
 import shutil
 import asyncio
-
 import requests
+
+import platform
+
+__version__ = "v0.2.0"
 
 
 HELP_TEXT = ("\
-Usage: vidsrc-search <command> [options]            \n\
+Usage: vidsrc-search <command> [option] [flags]     \n\
                                                     \n\
 Available commands:                                 \n\
     help        shows this menu                     \n\
     search      search a movie by name              \n\
     library     actions regarding the movie lib     \n\
                                                     \n\
-Use 'vidsrc-search help <command>' for info         \n\
-on a specific command.                              \
+Use 'vidsrc-search help <command>' for info on a    \n\
+specific command. Arguments are parsed strictly in  \n\
+the order above                                     \
 ")
 
+
 HELP_HELP = ("\
-Usage: vidsrc-search help <option>                  \n\
+Usage: vidsrc-search help [option]                  \n\
                                                     \n\
 Available options:                                  \n\
     help        shows detailed help for 'help'      \n\
@@ -31,23 +37,30 @@ Example:                                            \n\
      vidsrc-search help help                        \
 ")
 
+
 HELP_SEARCH = ("\
-Usage: vidsrc-search search <option>                \n\
+Usage: vidsrc-search search <option> [flags]        \n\
                                                     \n\
 Required option:                                    \n\
     <str>       a movie title that you would like   \n\
                 vidsrc-search to search for         \n\
                                                     \n\
 Optional flags:                                     \n\
-    --fallback  launch a movie through a custom     \n\
-                python window                       \n\
-                                                    \n\
-                                                    \n\
+    --raw       when this flag is specified, the    \n\
+                program won't do any preprocessing  \n\
+                to the html                         \n\
+    --new       when this flag is specified, the    \n\
+                program will re-cache the html to   \n\
+                your computer from vidsrc.to        \n\
                                                     \n\
 Example:                                            \n\
     vidsrc-search search 'oppenheimer'              \n\
-    vidsrc-search search 'avatar'                   \
+    vidsrc-search search 'avatar'                   \n\
+                                                    \n\
+The optional flags will have no effect with         \n\
+commands other than 'search'                        \
 ")
+
 
 HELP_LIB = ("\
 Usage: vidsrc-search library <option>               \n\
@@ -79,6 +92,13 @@ def show_help_search():
     
 def show_help_lib():
     print(HELP_LIB)
+    return
+
+
+def show_version():
+    print(f"vidsrc-search {__version__}")
+    print(f"{platform.python_implementation().lower()} {platform.python_version().lower()}")
+    print(f"{platform.platform(True, True).lower()} {platform.machine().lower()}")
     return
 
 
@@ -172,3 +192,45 @@ def bootstrap() -> None:
     make_directory(required_path_two)
     make_directory(required_path_three)
     return
+
+
+def load_json(path: str) -> list:
+    ret: list = []
+    path = os.path.expanduser(path)
+    if not os.path.exists(path):
+        return ret
+    with open(path, "r") as f:
+        try:
+            ret.append(json.load(f))
+            return ret
+        except json.JSONDecodeError:
+            return []
+
+
+def parse_entry(page: dict) -> list:
+    ret: list = []
+    for entry in page["result"]["items"]:
+        if "embed_url_imdb" in entry:
+            ret.append(entry)
+    return ret
+
+
+def unite_jsons(movie_index: int, tv_index: int) -> int:
+    print()
+    print(f" [Info] Uniting {movie_index + tv_index} json files")
+    unparsed_entries = []
+    parsed_entries = []
+    for i in range(movie_index):
+        unparsed_entries.append(load_json(f"~/.local/vidsrc-search/movie_buffer/{i}.json"))
+    for i in range(tv_index):
+        unparsed_entries.append(load_json(f"~/.local/vidsrc-search/tv_buffer/{i}.json"))
+
+    print(f" [Info] Parsing {movie_index + tv_index} json files")
+    for page in unparsed_entries:
+        parsed_entries += parse_entry(page[0])
+
+    print(" [Info] Dumping parsed json")
+    path = os.path.expanduser("~/.local/vidsrc-search/lib.json")
+    with open(path, "w") as f:
+        json.dump(parsed_entries, f)
+    return len(parsed_entries)
