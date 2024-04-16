5e832609b2db23fb77a337c91dacd241f92a8a42
Zichen <zichenziv@icloud.com>
2024-04-15 19:26:04 -0700

    Added debug logging, improved msg format, and added progress bar

--- src/vidsrc_search/core/library.py
+++ src/vidsrc_search/core/library.py
@@ -9,10 +9,25 @@ import asyncio
 import aiohttp
 import requests
 
-from typing import Any, List, Union
+from tqdm import tqdm
+from typing import Any, Dict, List, Union
 
 from .. import utils
+from ..utils import Logger
 from ..argparsing import ArgumentsError
+
+LogLibrary = Logger()
+
+
+class BarShell(tqdm):
+    """This class is an empty placeholder for when we do not
+    want to show the progress bar but still want the rest of the
+    program/module to work (e.g. when we are debug logging)
+    """
+    def __init__(*args, **kwargs) -> None: ...
+    def set_description_str(*args, **kwargs) -> None: ...
+    def update(*args, **kwargs) -> bool | None: ...
+    def close(*args, **kwargs) -> None: ...
 
 
 class Library:
@@ -26,12 +41,13 @@ class Library:
         exit_on_false: bool = True
     ) -> bool:
         """Checks whether the library exist or not"""
+        LogLibrary.log(f"checking if library exists")
         if os.path.exists(self.lib_path):
             return True
         if exit_on_false:
-            print("fatal: library does not exist")
-            print("fatal: please download the library first by using 'vidsrc-search library download'")
-            print("fatal: vidsrc-search terminating with exit code 2")
+            print(" • library does not exist")
+            print(" • please download the library first by using 'vidsrc-search library download'")
+            print(" • vidsrc-search terminating with exit code 2")
             sys.exit(2)
         return False
 
@@ -45,6 +61,9 @@ class DownloadManager:
         """Initializes a download manager instance with the types of media
         to be downloaded from vidsrc
         """
+        LogLibrary.log(f"initialized a downloadmanager instance")
+        self.progress: Union[BarShell, tqdm] = BarShell()
+
         self.types = types
         self.time: float
 
@@ -61,39 +80,61 @@ class DownloadManager:
     def start_download(self) -> None:
         """Starts the downloading of types specified in the constructor"""
         utils.check_internet()
+        if LogLibrary.emit == False:
+            print(" • initializing library download")
+            LogLibrary.log("checking whether terminal is a tty")
+            utils.check_tty()
+            self.progress = tqdm(
+                total=(len(self.types) * 14) + 3,
+                desc="initializing",
+                leave=False,
+                ascii="╺━",
+                bar_format=" • {desc} {bar} {percentage:.2f}% • {n_fmt}/{total} steps",
+                ncols=85
+            )
+        else:
+            LogLibrary.log("checking whether terminal is a tty")
+            utils.check_tty()
 
         start_time = time.time()
         for item in self.types:
-            size = DownloadManager.get_download_size(item)
-            asyncio.run(DownloadManager.download(size, item))
+            size = self.get_download_size(item)
+            asyncio.run(self.download(size, item))
 
             self.final_size += size * 15
             self.expected_size += utils.unite_jsons(f"~/.local/vidsrc-search/{item}_buffer/", f"~/.local/vidsrc-search/{item}.json")
         end_time = time.time()
 
+        self.progress.set_description_str("cleaning files")
         RemovalManager.remove_library()   # Removing previously downloaded library
         utils.unite_jsons(f"~/.local/vidsrc-search/", f"~/.local/vidsrc-search/lib.json", raw=False)
-        utils.cleanup()
-        self.time = start_time - end_time
+
+        self.progress.update(1)
+        self.time = end_time - start_time
+        self.progress.set_description_str("library download complete")
+        self.progress.close()
         return
 
     def print_summary(self) -> None:
         """Prints the summary of download"""
+        LogLibrary.log(f"printing summary")
         loss = 100 * ((self.expected_size - self.final_size) / self.expected_size)
         if loss < 1:
             loss = 0
-        print(f"info: total/estimated number of movies downloaded: {self.expected_size}/{self.final_size}")
-        print(f"info: estimated link loss from client/server connection issues: ~{loss:.2f}%")
-        print(f"info: operation took {self.time:.2f} seconds to complete")
-        print(f"info: library has been downloaded")
+        print(f" • total number of links to movies downloaded: {self.expected_size}")
+        print(f" • estimated link loss from connection issues: ~{loss:.2f}%")
+        print(f" • operation took {self.time:.2f} seconds to complete")
+        print(f" • vidsrc library has been downloaded")
         return
 
-    @staticmethod
-    def get_download_size(kind: str) -> int:
+    def get_download_size(self, kind: str) -> int:
         """Estimates the number of pages (links) of a download using binary
         search.
         """
-        print(f"info: estimating download size for {kind} items (this may take a while)")
+        self.progress.set_description_str(f"calculating {kind} items")
+        LogLibrary.log(f"starting download size estimation for {kind}")
+
+        requests_errors = 0
 
         index = 0
         width = 2048
@@ -101,9 +142,21 @@ class DownloadManager:
         last_direction = 1
         while width > 1:
             url = f"https://vidsrc.to/vapi/{kind}/new/{index}"
-            file = requests.get(url)
+            try:
+                file = requests.get(url)
+            except requests.RequestException as e:
+                LogLibrary.log(f"{type(e).__name__.lower()}: {str(e).lower()}")
+                LogLibrary.log(f"the above error occured during size estimation of {kind}")
+                requests_errors += 1
+                if requests_errors >= 10:
+                    print(f" • too many errors during size estimation of {kind}")
+                    print(f" • vidsrc-search terminating with error code 16")
+                    sys.exit(16)
+                continue
             if last_direction != this_direction:
+                self.progress.update(1)
                 width = width // 2
+                LogLibrary.log(f"link estimation improved: now at ±{width}")
             if len(file.content) > 50:
                 last_direction = this_direction
                 this_direction = 1
@@ -120,29 +173,32 @@ class DownloadManager:
         async with session.get(url) as response:
             if response.content_type == "application/json":
                 if _tries != 3:
-                    print(f"info: retry successful")
+                    LogLibrary.log(f"retry succesful")
                 return await response.json()
+            LogLibrary.log(f"bad connection detected: retrying ({_tries})")
             if _tries != 0:
-                print(f"error: bad connection encountered, retrying ({_tries})")
                 return await DownloadManager.fetch_downloads(session, url, _tries - 1)
-            print(f"info: too many retries, omitting current link")
+            LogLibrary.log(f"too many retries: omitting current link")
             return
 
-    @staticmethod
-    async def download(total: int, type: str):
+    async def download(self, total: int, type: str):
         """Downloads the movie library into a buffer folder on disk"""
         domain = f"https://vidsrc.to/vapi/{type}/new/"
         folder = f"~/.local/vidsrc-search/{type}_buffer/"
         urls = [f"{domain}{i}" for i in range(total)]
-        print(f"info: initialized {type} download with {total} links to jsons")
-        print(f"info: waiting for response(s) from {total} server requests")
+        LogLibrary.log(f"initiating downloads with {total} links to {type}s")
 
+        self.progress.update(1)
         async with aiohttp.ClientSession() as session:
+            self.progress.set_description_str("waiting for server responses")
+            LogLibrary.log(f"adding fetch_downloads() tasks to task list")
             tasks = [DownloadManager.fetch_downloads(session, url) for url in urls]
+            LogLibrary.log(f"awaiting tasks")
             jsons = await asyncio.gather(*tasks)
             folder = os.path.expanduser(folder)
 
-            print(f"info: writing json files to disk")
+            self.progress.update(1)
+            LogLibrary.log(f"writing json files")
             for index, file in enumerate(jsons):
                 f = open(f"{folder}{index}.json", "w")
                 f.write(json.dumps(file))
@@ -156,18 +212,21 @@ class RemovalManager:
     @staticmethod
     def remove_library() -> None:
         """Removes the library downloaded from vidsrc"""
+        LogLibrary.log(f"handling library removal")
         utils.rmfile(Library.lib_path)
         return
 
     @staticmethod
     def remove_cache() -> None:
         """Removes the cached html storage"""
+        LogLibrary.log(f"handling cache removal")
         utils.rmdir_recurse(Library.cache_path)
         return
 
     @staticmethod
     def remove_buffer() -> None:
         """Removes the buffer storage"""
+        LogLibrary.log(f"handling buffer removal")
         folders = utils.get_file(f"~/.local/vidsrc-search", "_buffer")
         for folder in folders:
             utils.rmdir_recurse(f"~/.local/vidsrc-search/" + folder)
@@ -175,21 +234,25 @@ class RemovalManager:
 
     def handle_remove_library(self) -> None:
         """Handles the removal of library"""
+        LogLibrary.log(f"initiating handle_remove_libary()")
         if not os.path.exists(Library.lib_path):
-            print("fatal: library does not exist")
-            print("fatal: please download the library first by using 'vidsrc-search library download'")
-            print("fatal: vidsrc-search terminating with exit code 2")
+            print(" • library does not exist")
+            print(" • please download the library first by using 'vidsrc-search library download'")
+            print(" • vidsrc-search terminating with exit code 2")
             sys.exit(2)
         with open(Library.lib_path, "r") as f:
+            LogLibrary.log(f"loading library json for movie link estimation")
             jsons = json.load(f)
+
+        LogLibrary.log("checking whether terminal is a tty")
+        utils.check_tty()
 
         confirm = input(f" > are you sure you want to remove ~{len(jsons) * 15} links to movies? (Y/n) ")
         if not confirm == "Y":
-            print("info: user declined operation")
-            print("info: vidsrc-search terminating per user request")
+            print(" • user declined operation")
+            print(" • vidsrc-search terminating per user request")
             sys.exit(0)
-        print(f"info: removing downloaded library")
-        print(f"info: removing cached html files")
+        print(f" • removing library and cache files")
         RemovalManager.remove_library()
         RemovalManager.remove_cache()
         return
@@ -200,26 +263,30 @@ class SizeManager:
 
     def __init__(self) -> None:
         """Initializes a SizeManager by calculating the library size"""
+        LogLibrary.log(f"initializing sizemanager class")
         self.size = utils.get_folder_size_recursive(Library.root_path)
         return
 
     def print_size(self) -> None:
-        print(f"total disk usage by vidsrc-search: {self.size}")
+        print(f" • total disk usage by vidsrc-search: {self.size}")
         return
 
 
-def run_module(arguments: List[str]) -> None:
+def run_module(modules: List[str], args: Dict[str, Any]) -> None:
     """Runs the library module"""
-    if arguments == ["library", "download"]:
+    if args["dbg"]:
+        LogLibrary.change_emit_level(True)
+        LogLibrary.log(f"arguments received by library: {modules}")
+    if modules == ["library", "download"]:
         downloader = DownloadManager(["movie", "tv"])
         downloader.handle_download_library()
         return
-    if arguments == ["library", "remove"]:
+    if modules == ["library", "remove"]:
         remover = RemovalManager()
         remover.handle_remove_library()
         return
-    if arguments == ["library", "size"]:
+    if modules == ["library", "size"]:
         sizer = SizeManager()
         sizer.print_size()
         return
-    raise ArgumentsError(f"invalid arguments for command 'library' received: {' '.join(arguments)}")
+    raise ArgumentsError(f"invalid arguments for command 'library' received: {' '.join(modules)}")
